71363d5936396875c471a50f7f84860c4b7bb815
Jakub Smolik <smolikja@protonmail.com>
2024-07-29 15:48:19 +0200

    wip: firebase auth flow connection

---
+++ lib/firebase_auth_flow.dart
@@ -0,0 +1,7 @@
+library firebase_auth_flow;
+
+export 'package:firebase_auth_flow/presentation/firebase_auth_flow.dart'
+    show
+        FirebaseAuthFlow,
+        FirebaseAuthFlowProvider,
+        FirebaseAuthFlowDependencies;
---
+++ lib/presentation/firebase_auth_flow.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth_flow/presentation/auth_page_content.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum FirebaseAuthFlowProvider { email }
+
+class FirebaseAuthFlowDependencies {
+  const FirebaseAuthFlowDependencies({
+    required this.provider,
+    required this.activityIndicator,
+    this.disabledOpacity = 0.65,
+    this.borderRadius = 12.0,
+  });
+
+  final FirebaseAuthFlowProvider provider;
+  final Widget activityIndicator;
+  final double disabledOpacity;
+  final double borderRadius;
+  // TODO: color scheme
+}
+
+class FirebaseAuthFlow extends ConsumerStatefulWidget {
+  const FirebaseAuthFlow(this.dep, {super.key});
+
+  /// FirebaseAuthFlow dependencies
+  final FirebaseAuthFlowDependencies dep;
+
+  @override
+  ConsumerState<FirebaseAuthFlow> createState() => _FirebaseAuthFlowState();
+}
+
+class _FirebaseAuthFlowState extends ConsumerState<FirebaseAuthFlow> {
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      child: Scaffold(
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            AbsorbPointer(
+              absorbing: false, // TODO: z provideru
+              child: AuthPageContent(widget.dep),
+            ),
+            if (true)
+              Center(child: widget.dep.activityIndicator), // TODO: z provideru
+          ],
+        ),
+      ),
+    );
+  }
+}
