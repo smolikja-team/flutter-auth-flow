e1da1c1c08b535538d57d1a252aa5fa84da1f456
Jakub Smolik <smolikja@protonmail.com>
2025-05-04 12:46:47 +0200

    cleaning

--- lib/src/core/text_styles/text_styles.dart
+++ lib/src/core/text_styles/text_styles.dart
@@ -5,28 +5,10 @@ import 'package:flutter/material.dart';
 class TextStyles {
   // MARK: HEADLINE STYLES
 
-  // Headline Large - Regular (32px)
-  static TextStyle get headlineLarge => const TextStyle(
-        fontSize: 32,
-        fontWeight: FontWeight.w400,
-      );
-
-  // Headline Large Bold - Bold (32px)
-  static TextStyle get headlineLargeBold => const TextStyle(
-        fontSize: 32,
-        fontWeight: FontWeight.bold,
-      );
-
   // Headline Medium - Regular (28px)
   static TextStyle get headlineMedium => const TextStyle(
         fontSize: 28,
         fontWeight: FontWeight.w400,
-      );
-
-  // Headline Medium Bold - Bold (28px)
-  static TextStyle get headlineMediumBold => const TextStyle(
-        fontSize: 28,
-        fontWeight: FontWeight.bold,
       );
 
   // Headline Small Bold - Bold (24px)
@@ -35,38 +17,12 @@ class TextStyles {
         fontWeight: FontWeight.bold,
       );
 
-  // MARK: DISPLAY STYLES
-
-  // Display Large - Regular (57px)
-  static TextStyle get displayLarge => const TextStyle(
-        fontSize: 57,
-        fontWeight: FontWeight.w400,
-      );
-
   // MARK: TITLE STYLES
-
-  // Title Large - Regular (22px)
-  static TextStyle get titleLarge => const TextStyle(
-        fontSize: 22,
-        fontWeight: FontWeight.w400,
-      );
-
-  // Title Large Bold - Bold (22px)
-  static TextStyle get titleLargeBold => const TextStyle(
-        fontSize: 22,
-        fontWeight: FontWeight.bold,
-      );
 
   // Title Medium - Medium (16px)
   static TextStyle get titleMedium => const TextStyle(
         fontSize: 16,
         fontWeight: FontWeight.w500,
-      );
-
-  // Title Medium Semibold - Semibold (16px)
-  static TextStyle get titleMediumSemibold => const TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w600,
       );
 
   // Title Small - Medium (14px)
@@ -101,43 +57,7 @@ class TextStyles {
         fontWeight: FontWeight.w600,
       );
 
-  // Label Large Error - Medium (14px)
-  static TextStyle get labelLargeError => const TextStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w500,
-      );
-
-  // Label Large Hyperlink - Medium (14px)
-  static TextStyle get labelLargeHyperlink => const TextStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w500,
-      );
-
-  // Label Large Disabled - Medium (14px)
-  static TextStyle get labelLargeDisabled => const TextStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w500,
-      );
-
   // MARK: BODY STYLES
-
-  // Body Large - Regular (16px)
-  static TextStyle get bodyLarge => const TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w400,
-      );
-
-  // Body Large Bold - Bold (16px)
-  static TextStyle get bodyLargeBold => const TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.bold,
-      );
-
-  // Body Large Error - Regular (16px)
-  static TextStyle get bodyLargeError => const TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w400,
-      );
 
   // Body Medium - Regular (14px)
   static TextStyle get bodyMedium => const TextStyle(
@@ -147,12 +67,6 @@ class TextStyles {
 
   // Body Small - Regular (12px)
   static TextStyle get bodySmall => const TextStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.w400,
-      );
-
-  // Body Small with reduced opacity - Regular (12px)
-  static TextStyle get bodySmallDisabled => const TextStyle(
         fontSize: 12,
         fontWeight: FontWeight.w400,
       );
